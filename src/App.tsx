fe68826d51d7a8803fb76ebeda806012d9fab7dc
Orzelius <albertkostusev@gmail.com>
2021-03-08 16:19:25 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="" />
+      <img src={logo} className="App-logo w-10" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
