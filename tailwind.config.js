aa465ef6302f894beeca313c29b4f4c63e1ac6e2
Orzelius <albertkostusev@gmail.com>
2021-03-15 11:14:42 +0200

    I'm dumb

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,7 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 
     options: {
-      whitelistPatterns: [
-        "/^bg-coolGray-/",
-        "/^bg-red-/",
-        "/^bg-amber-/",
-        "/^bg-emerald-/",
-        "/^bg-blue-/",
-        "/^bg-indigo-/",
-        "/^bg-violet-/",
-        "/^bg-pink-/",
-      ],
+      whitelistPatterns: [/^bg-/],
     },
   },
   darkMode: false, // or 'media' or 'class'
